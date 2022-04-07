a0929d2edc6df6b637c5f0e4343eced9f7388001
FlutLab.io <>
2022-04-07 15:46:46 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,19 +10,50 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: ListView(
+          padding: const EdgeInsets.all(10.0),
           children: [
             ListTile(
-              title: Row(children: [
-                Icon(Icons.info_outline_rounded),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: Text(
-                    'About',
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                title: Row(children: [
+                  Icon(Icons.info_outline_rounded),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      'About',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-              ]),
-            ),
+                ]),
+                onTap: () {
+                  Navigator.pushNamed(context, '/about');
+                }),
+            ListTile(
+                title: Row(children: [
+                  Icon(Icons.file_copy_rounded),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      'About',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ]),
+                onTap: () {
+                  Navigator.pushNamed(context, '/portofolio');
+                }),
+            ListTile(
+                title: Row(children: [
+                  Icon(Icons.phone_callback_rounded),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      'About',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ]),
+                onTap: () {
+                  Navigator.pushNamed(context, '/contact');
+                }),
           ],
         ),
         // Column(
